1777f1bd638b36e6b6aa32e60a745a1b4491624e
John Marsh <john.marsh@gmail.com>
2018-11-25 10:21:40 -0500

    New version of the sign script
    Fixed several bugs and changed some timings around

--- wildersign.ino
+++ wildersign.ino
@@ -8,6 +8,7 @@
 uint16_t state;
 long loopcount;
 long randNumber;
+long wipeNumber;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_RGBW + NEO_KHZ800);
 
@@ -41,14 +42,14 @@ void loop() {
     case 0:
       //add to the counter
       loopcount += 1;
-      if(loopcount >= 3000) {
+      if(loopcount >= 1800) {  //3000 is about 5 minutes, 600 is a minute, 1800 is 3 minutes
         state = 1;
       }
       delay(100);
       break;
     case 1:
       //pick a random display effect
-      randNumber = random(18);
+      randNumber = random(21);
       switch (randNumber) {
         case 0:
           rainbowCycle(20);
@@ -63,46 +64,55 @@ void loop() {
           theaterChase(strip.Color(0, 255, 0), 60); // green
           break;
         case 4:
-          theaterChase(strip.Color(0, 0, 0, 255), 60); // white
+          rainbowCycle(20);
           break;
         case 5:
-          theaterChase(strip.Color(255, 0, 255, 0), 60); // purple
+          theaterChase(strip.Color(0, 0, 0, 255), 60); // white
           break;
         case 6:
-          theaterChase(strip.Color(255, 255, 0, 0), 60); // yellow
+          theaterChase(strip.Color(255, 0, 255, 0), 60); // purple
           break;
         case 7:
-          theaterChase(strip.Color(0, 255, 255, 0), 60); // aqua
+          theaterChase(strip.Color(255, 255, 0, 0), 60); // yellow
           break;
         case 8:
-          theaterChase(strip.Color(random(0,255), random(0,255), random(0,255)), random(50,101)); // random color and speed
+          theaterChase(strip.Color(0, 255, 255, 0), 60); // aqua
           break;
         case 9:
-          CylonBounce(strip.Color(0, 0, 0, 255), 4, 10, 50); //White
+          rainbowCycle(20);
           break;
         case 10:
-          CylonBounce(strip.Color(255, 0, 255, 0), 4, 10, 50); //purple
+          theaterChase(strip.Color(random(0,255), random(0,255), random(0,255)), random(50,101)); // random color and speed
           break;
         case 11:
-          CylonBounce(strip.Color(0, 0, 255, 0), 4, 10, 50); //blue
+          CylonBounce(strip.Color(0, 0, 0, 255), 4, 10, 50); //White
           break;
         case 12:
-          CylonBounce(strip.Color(255, 0, 0, 0), 4, 10, 50); //red
+          CylonBounce(strip.Color(255, 0, 255, 0), 4, 10, 50); //purple
           break;
         case 13:
-          CylonBounce(strip.Color(0, 255, 0, 0), 4, 10, 50); //green
+          CylonBounce(strip.Color(0, 0, 255, 0), 4, 10, 50); //blue
           break;
         case 14:
-          CylonBounce(strip.Color(0, 255, 255, 0), 4, 10, 50); //yellow
+          CylonBounce(strip.Color(255, 0, 0, 0), 4, 10, 50); //red
           break;
         case 15:
-          CylonBounce(strip.Color(random(0,255), random(0,255), random(0,255), 0), random(2,6), random(10,26), random(50,101)); //random color, size, and speed
+          rainbowCycle(20);
           break;
         case 16:
-          Sparkle(strip.Color(0, 0, 0, 255), 5); //do a stadium photography effect
+          CylonBounce(strip.Color(0, 255, 0, 0), 4, 10, 50); //green
           break;
         case 17:
-          Sparkle(strip.Color(random(50,255), random(50,255), random(50,255), 0), random(0,10)); //do a random color stadium photography effect
+          CylonBounce(strip.Color(0, 255, 255, 0), 4, 10, 50); //yellow
+          break;
+        case 18:
+          CylonBounce(strip.Color(random(0,255), random(0,255), random(0,255), 0), random(2,6), random(10,26), random(50,101)); //random color, size, and speed
+          break;
+        case 19:
+          Sparkle(strip.Color(0, 0, 0, 255), 10); //do a stadium photography effect
+          break;
+        case 20:
+          Sparkle(strip.Color(random(50,255), random(50,255), random(50,255), 0), random(10,25)); //do a random color stadium photography effect
           break;
       }
       state = 2;
@@ -112,7 +122,20 @@ void loop() {
       state = 0;
       loopcount = 0;
 
-      colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+      
+      wipeNumber = random(2);
+      switch (wipeNumber) {
+        case 0:
+          colorWipe(strip.Color(0, 0, 0, 255), 40); // White RGBW
+          break;
+        case 1:
+          colorWipeReverse(strip.Color(0, 0, 0, 255), 40); // White RGBW
+          break;
+        default:
+          colorWipe(strip.Color(0, 0, 0, 255), 40); // White RGBW
+          break;
+      }
+      
       
       //for(uint16_t i=0; i<strip.numPixels(); i++) {
       //  strip.setPixelColor(i, strip.Color(0, 0, 0, 255));
@@ -150,17 +173,17 @@ void setAll(uint32_t c) {
   strip.show();
 }
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
+//void rainbow(uint8_t wait) {
+//  uint16_t i, j;
+//
+//  for(j=0; j<256; j++) {
+//    for(i=0; i<strip.numPixels(); i++) {
+//      strip.setPixelColor(i, Wheel((i+j) & 255));
+//    }
+//    strip.show();
+//    delay(wait);
+//  }
+//}
 
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
@@ -229,9 +252,9 @@ uint32_t Wheel(byte WheelPos) {
 void CylonBounce(uint32_t c, int EyeSize, int SpeedDelay, int ReturnDelay){
 
   long loopstodo;
-  loopstodo = random(1,5);
+  loopstodo = random(3,8);
 
-  for(int i = 0; i < loopstodo; i++) {
+  for(int b = 0; b < loopstodo; b++) {
     for(int i = 0; i < strip.numPixels()-EyeSize-2; i++) {
       
       setAll(strip.Color(0, 0, 0, 0));
@@ -263,11 +286,19 @@ void CylonBounce(uint32_t c, int EyeSize, int SpeedDelay, int ReturnDelay){
 }
 
 void Sparkle(uint32_t c, int SpeedDelay) {
-  int Pixel = random(strip.numPixels());
-  strip.setPixelColor(Pixel, c);
-  strip.show();
-  delay(SpeedDelay);
-  strip.setPixelColor(Pixel, strip.Color(0, 0, 0, 0));
+
+  long sparklestodo;
+  sparklestodo = random(150,301);
+
+  setAll(strip.Color(0, 0, 0, 0));
+
+  for(int i = 0; i < sparklestodo; i++) {
+    int Pixel = random(strip.numPixels());
+    strip.setPixelColor(Pixel, c);
+    strip.show();
+    delay(SpeedDelay);
+    strip.setPixelColor(Pixel, strip.Color(0, 0, 0, 0));
+  }
 }
 
 
